b50a9c7862c5ac3bb98675efaa18d76e3dd2bdc2
Jay Patel <86232687+JayP52@users.noreply.github.com>
2021-08-13 12:04:11 +0530

    added lobby

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PocketPackage
 
-A description of this package.
+A new description of this package.
---
+++ Sources/PocketPackage/LobbyView.swift
@@ -0,0 +1,15 @@
+//
+//  LobbyView.swift
+//  
+//
+//  Created by Jay Patel on 13/08/21.
+//
+
+import SwiftUI
+
+public struct LobbyView: View {
+    public  init() {}
+    public var body: some View {
+        Text("Lobby")
+    }
+}
